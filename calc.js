0e66a014098f8718ec9f676eeaf897407b4470b2
zdicino <zdicino@asu.edu>
2016-07-14 14:55:09 -0700

    Andrew is an idiot
    re-committing

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 // Get all the keys from document
 var keys = document.querySelectorAll('#calculator span');
-var operators = ['+', '-', 'x', '/'];
+var operators = ['+', '-', 'x', '÷'];
 var decimalAdded = false;
 
 // Add onclick event to all the keys and perform operations
@@ -18,12 +18,6 @@ for(var i = 0; i < keys.length; i++) {
 			decimalAdded = false;
 		}
 		
-		else if(btnVal == 'bksp'){
-			var str = input.innerHTML;
-			if(str.length > 0){
-				input.innerHTML = str.substring(0, str.length-1);
-			}
-		}
 		// If eval key is pressed, calculate and display the result
 		else if(btnVal == '=') {
 			var equation = inputVal;
@@ -37,7 +31,7 @@ for(var i = 0; i < keys.length; i++) {
 				equation = equation.replace(/.$/, '');
 			
 			if(equation)
-				input.innerHTML = math.eval(equation);
+				input.innerHTML = eval(equation);
 				
 			decimalAdded = false;
 		}
@@ -80,9 +74,6 @@ for(var i = 0; i < keys.length; i++) {
 			}
 		}
 		
-		else if(btnVal == 'sqrt'){
-			input.innerHTML += btnVal + '(';
-		}
 		// if any other key is pressed, just append it
 		else {
 			input.innerHTML += btnVal;
@@ -92,3 +83,35 @@ for(var i = 0; i < keys.length; i++) {
 		e.preventDefault();
 	} 
 }
+
+//converting pixels to em
+function pxToEm(size_px, parent_size){
+	var result = size_px/parent_size;
+	return result;
+}
+
+//converting em to pixels
+function emToPx(size_em, parent_size){
+	var result = size_em * parent_size;
+	return result;
+}
+
+//converting pixels to %
+function pxToPercent(size_px, parent_size){
+	var result = (size_px/parent_size) * 100;
+	return result;
+}
+
+//dpi calculation
+//diagonal size must be in inches
+//width and length in pixels
+function calc_dpi(px_width, px_length, diagonal_size){
+	//calculate the diagonal resolution
+	var width_square = Math.pow(px_width, 2);
+	var length_square = Math.pow(px_length, 2);
+	var resolution = Math.sqrt(width_square + length_square);
+	//use diagonal resolution to calculate dpi
+	var dpi = resolution/diagonal_size;
+	return dpi;
+}
+
